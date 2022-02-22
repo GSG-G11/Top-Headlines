7c301c8e405431e54e6386ab39d4705869e5a4a2
Ibrahim-Jarada <ibrahim.jarada@gmail.com>
2022-02-22 22:58:05 +0200

    initialize .env and use it to get port, modifiy express.static path import and apply router apply json and urlencoded middleware Relates #11

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
-const express = require("express");
-const path = require("path");
+require('env2')('.env');
+const express = require('express');
+const { join } = require('path');
+const router = require('./routes');
+
 const app = express();
-
-app.use(express.static('public'));
-
+app.set('port', process.env.PORT || 3000);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(join(__dirname, '..', 'public')));
+app.use(router);
 module.exports = app;
