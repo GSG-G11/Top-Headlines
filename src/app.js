757bc035c08abf78907f49ffac70ed07042bffa7
yousra Jaradh <yousrakhaleel@gmail.com>
2022-02-22 17:04:47 +0300

    edit in app.js file

--- src/app.js
+++ src/app.js
@@ -2,6 +2,6 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.listen(3000, () => {
-  console.log("App running on port 3000");
-});
+app.use(express.static('public'));
+
+module.exports = app;
