2c7c30eaea3ce60313d37c31871bfc897b9cd0ed
Ibrahim-Jarada <ibrahim.jarada@gmail.com>
2022-02-22 23:03:27 +0200

    install and apply compression middleware Relates #11

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 require('env2')('.env');
 const express = require('express');
 const { join } = require('path');
+const compression = require('compression');
 const router = require('./routes');
 
 const app = express();
 app.set('port', process.env.PORT || 3000);
+app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(join(__dirname, '..', 'public')));
