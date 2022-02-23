614be16b4f6c8d3f3549fd324b77410b9ac72885
yousra Jaradh <yousrakhaleel@gmail.com>
2022-02-23 13:41:36 +0300

    add handle search Relates #15

--- src/controllers/getCategory.js
+++ src/controllers/getCategory.js
@@ -1,6 +1,5 @@
 const fetch = require('node-fetch');
 const { serverError } = require('./error');
-const apiKey = process.env.apiKey;
 const getCategory = (category) => (req, res, next) => {
   fetch(
     `https://newsapi.org/v2/top-headlines?category=${category}&apiKey=${process.env.apiKey}`,
--- src/controllers/handleSearch.js
+++ src/controllers/handleSearch.js
@@ -1,5 +1,4 @@
 const fetch = require('node-fetch');
-const apiKey = process.env.apiKey;
 const handleSearch = (req, res) => {
  fetch(`https://newsapi.org/v2/everything?q=${req.body.keyword}&apiKey=${process.env.apiKey}`)
  .then((data) => data.json())
