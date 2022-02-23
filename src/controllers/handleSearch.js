efa671d04328f0efc2b711f379dd9ae4dd678011
yousra Jaradh <yousrakhaleel@gmail.com>
2022-02-23 13:30:39 +0300

    add handle search Relates #15

--- src/controllers/handleSearch.js
+++ src/controllers/handleSearch.js
@@ -1,7 +1,9 @@
-const handleSearch = (req, res, next) => {
-  console.log(req.body);
-  res.send(req.body);
-  next();
+const fetch = require('node-fetch');
+
+const handleSearch = (req, res) => {
+ fetch(`https://newsapi.org/v2/everything?q=${req.body.keyword}&apiKey=9c974a740f55477fb7c99c9f4376ad43`)
+ .then((data) => data.json())
+ .then((result) => res.json(result))
 };
 
 module.exports = handleSearch;
