39d8af0a9788ab3269d7a83afaa07e7275257c82
Ibrahim-Jarada <ibrahim.jarada@gmail.com>
2022-02-23 18:35:41 +0200

    add some routes tests Relates #29

--- src/test.js
+++ src/test.js
@@ -1,37 +1,59 @@
 const requset = require('supertest');
 const app = require('./app');
 
-describe ('Testing Static GET', () =>{
-    test('/route' , (done) => {
-        requset(app)
-        .get('/')
-        .expect(200)
-        .expect('content-type',/html/)
-        .end((err) =>{
-            if (err) return done(err);
-            return done();
-        })
-    });
+describe('Testing Static GET', () => {
+  test('/route', (done) => {
+    requset(app)
+      .get('/')
+      .expect(200)
+      .expect('content-type', /html/)
+      .end((err) => {
+        if (err) return done(err);
+        return done();
+      });
+  });
 
-    test('not found' , (done) => {
-        requset(app)
-        .get('/notfound')
-        .expect(404)
-        .expect('content-type',/html/)
-        .end((err) =>{
-            if (err) return done(err);
-            return done();
-        })
-    });
+  test('not found', (done) => {
+    requset(app)
+      .get('/notfound')
+      .expect(404)
+      .expect('content-type', /html/)
+      .end((err) => {
+        if (err) return done(err);
+        return done();
+      });
+  });
 
-    test('search' , (done) => {
-        requset(app)
-        .post('/search')
-        .expect(200)
-        .expect('content-type',/json/)
-        .end((err ,res) =>{
-            if (err) return done(err);
-            return done();
-        })
-    });
-})
+  test('search route POST', (done) => {
+    requset(app)
+      .post('/search')
+      .expect(200)
+      .expect('content-type', /json/)
+      .end((err, res) => {
+        if (err) return done(err);
+        return done();
+      });
+  });
+
+  test('search route GET', (done) => {
+    requset(app)
+      .get('/search')
+      .expect(200)
+      .expect('content-type', /html/)
+      .end((err, res) => {
+        if (err) return done(err);
+        return done();
+      });
+  });
+
+  test('/category route', (done) => {
+    requset(app)
+      .get('/category')
+      .expect(200)
+      .expect('content-type', /html/)
+      .end((err) => {
+        if (err) return done(err);
+        return done();
+      });
+  });
+});
