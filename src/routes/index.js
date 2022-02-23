14473698631a210f7296f7ac919cba4086db8949
Ibrahim-Jarada <ibrahim.jarada@gmail.com>
2022-02-23 18:55:06 +0200

    fix error pages Relates #31

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const category = require('./category');
-const { handleSearch, handleSearchPage } = require('../controllers');
+const { handleSearch, handleSearchPage,notFoundError,serverError} = require('../controllers');
 
 router.use('/category', category);
 router.post('/search', handleSearch);
 router.get('/search', handleSearchPage);
+router.use(notFoundError)
+router.use(serverError)
 
 module.exports = router;
